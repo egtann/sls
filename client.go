03119fb10aeeba3d6414a3a4c87abd8ff3ec1bca
Evan Tann <egtann@gmail.com>
2018-11-03 16:14:37 -0700

    improve client api

--- client.go
+++ client.go
@@ -13,32 +13,45 @@ import (
 
 // Client is used to interact with the logging server.
 type Client struct {
-	client *http.Client
-	apiKey string
-	url    string
-	buf    []string
-	mu     sync.RWMutex
-	errCh  chan<- error
+	client        *http.Client
+	apiKey        string
+	url           string
+	buf           []string
+	mu            sync.RWMutex
+	errCh         chan error
+	flushInterval time.Duration
 }
 
 // NewClient for interacting with sls.
-func NewClient(url, apiKey string, flushInterval time.Duration) *Client {
+func NewClient(url, apiKey string) *Client {
 	c := &Client{
 		client: &http.Client{Timeout: 2 * time.Second},
 		url:    url,
 		apiKey: apiKey,
 	}
-	go func() {
-		for range time.Tick(flushInterval) {
-			c.flush()
-		}
-	}()
 	return c
 }
 
+// WithFlushInterval specifies how long to wait before flushing the buffer to
+// the log server. This returns a function which flushes the client and should
+// be called with defer before main exits.
+func (c *Client) WithFlushInterval(dur time.Duration) (*Client, func()) {
+	c.flushInterval = dur
+	go func() {
+		for range time.Tick(dur) {
+			c.flush()
+		}
+	}()
+	return c, c.flush
+}
+
+// marshalBuffer to JSON. If the buffer is empty, marshalBuffer reports nil.
 func (c *Client) marshalBuffer() ([]byte, error) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	if len(c.buf) == 0 {
+		return nil, nil
+	}
 	byt, err := json.Marshal(c.buf)
 	c.buf = []string{}
 	if err != nil {
@@ -47,6 +60,8 @@ func (c *Client) marshalBuffer() ([]byte, error) {
 	return byt, nil
 }
 
+// flush the log buffer to the server. This happens automatically over time if
+// WithFlushInterval is called.
 func (c *Client) flush() {
 	byt, err := c.marshalBuffer()
 	if err != nil {
@@ -74,9 +89,12 @@ func (c *Client) flush() {
 	}
 }
 
-func (c *Client) WithErrorChannel(ch chan<- error) *Client {
-	c.errCh = ch
-	return c
+// Err is a convenience function that wraps an error channel.
+func (c *Client) Err() <-chan error {
+	if c.errCh == nil {
+		c.errCh = make(chan error)
+	}
+	return c.errCh
 }
 
 func (c *Client) sendErr(err error) {
@@ -87,16 +105,21 @@ func (c *Client) sendErr(err error) {
 }
 
 // Log to sls. This is threadsafe.
-func (c *Client) Log(buf []string) {
+func (c *Client) Log(s string) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
-	c.buf = append(c.buf, buf...)
+	if c.flushInterval > 0 {
+		c.buf = append(c.buf, s)
+		return
+	}
+	c.buf = []string{s}
+	c.flush()
 }
 
 // Write satisfies the io.Writer interface, so a client can be a drop-in
 // replacement for stdout. Logs are written to the external service on a
 // best-effort basis.
 func (c *Client) Write(byt []byte) (int, error) {
-	c.Log([]string{string(byt)})
+	c.Log(string(byt))
 	return len(byt), nil
 }
